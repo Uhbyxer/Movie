11cc738ffe92a3ced26e228435f64bfd16061855
Taras_Hrynchuk <Taras_Hrynchuk@epam.com>
2016-02-07 15:03:49 +0200

    user birth

--- src/main/java/com/epam/spring/movie/bean/User.java
+++ src/main/java/com/epam/spring/movie/bean/User.java
@@ -1,9 +1,15 @@
 package com.epam.spring.movie.bean;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 
 public class User extends NamedBean {
 
 	private String email;
+	
+	private LocalDate birth;
 	
 	public String getEmail() {
 		return email;
@@ -12,10 +18,23 @@ public class User extends NamedBean {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public LocalDate getBirth() {
+		return birth;
+	}
+	
+	public void setBirthDate(java.util.Date date) {
+		birth = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalDate();
+	}
+	
+	public void setBirth(LocalDate birth) {
+		this.birth = birth;
+	}
 
 	@Override
 	public String toString() {
-		return name + " <" + email + ">";
+		return name + " <" + email + ">" 
+					+ (birth == null ? "" : " (birth: " + birth + ")");
 	}
 	
 }
