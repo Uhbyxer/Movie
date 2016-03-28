47a4749b188d24653ba3a60f9139f3f19a6a429b
taras <tgrynchuk@gmail.com>
2016-03-28 18:51:08 +0300

    classpath:/spring.xml

--- src/test/java/com/epam/spring/movie/AbstractTestCase.java
+++ src/test/java/com/epam/spring/movie/AbstractTestCase.java
@@ -5,6 +5,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/conf/spring.xml")
 @ContextConfiguration("classpath:spring.xml")
 public abstract class AbstractTestCase {
 
