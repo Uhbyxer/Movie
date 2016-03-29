9419631e64e09285756ed518e6ff555deae7e4a9
Taras_Hrynchuk <Taras_Hrynchuk@epam.com>
2016-03-30 02:14:18 +0300

    add template

--- src/main/java/com/epam/spring/movie/mvc/EventController.java
+++ src/main/java/com/epam/spring/movie/mvc/EventController.java
@@ -1,26 +1,47 @@
 package com.epam.spring.movie.mvc;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.epam.spring.movie.bean.Event;
 import com.epam.spring.movie.service.EventService;
 
 @Controller
+//@RequestMapping(value = { "/", "/events" })
 public class EventController {
 
 	@Autowired
 	private EventService eventService;
 
 	
-	@RequestMapping(value = { "/", "/events" }, method = RequestMethod.GET)
-	public String getAllEvents(Model model) {
+	//..., headers = "accept/text/*"
+	//..., params = {"find=ByMake", "form"}
+	
+//	@RequestMapping(value = { "/", "/events" }, method = RequestMethod.GET)
+//	public String getAllEvents(Model model) {
+//		List<Event> events = eventService.getAll();
+//		model.addAttribute("events", events);
+//		return "events";
+//	}
+//	
+
+	@RequestMapping(value = { "/", "/events" })
+	public ModelAndView getAllEvents(HttpServletRequest request) {
+		
+		//String foo = request.getParameter("foo");
+		
 		List<Event> events = eventService.getAll();
-		model.addAttribute("events", events);
-		return "events";
+		ModelAndView mav = new ModelAndView("events");
+		mav.addObject("events", events);
+		
+		return mav;
 	}
+
+	
 }
